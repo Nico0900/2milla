57db9d786451858a24e65f106c8c46c278c1f6d2
Nico0900 <nv0900nico@gmail.com>
2025-07-29 15:16:27 -0400

    up
    up

--- js/button.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('button').addEventListener('mouseover', function () {
-    this.style.transform = 'scale(1.08) rotate(-2deg)';
-});
-document.querySelector('button').addEventListener('mouseout', function () {
-    this.style.transform = '';
-});
